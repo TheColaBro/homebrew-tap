8d10909e8a5620d56746c1a33ccb89bb0f4369fa
Ming Chen <mchen.uob@gmail.com>
2026-08-17 11:05:19 +0100

    fix(cask): use precise repo URL casing

--- Casks/vocabfloat.rb
+++ Casks/vocabfloat.rb
@@ -2,10 +2,10 @@ cask "vocabfloat" do
   version "1.0.0"
   sha256 "37679ac785ef5e86a201583a85eba960f0ae745a52bb92dfed5dca01ab1dbbdb"
 
-  url "https://github.com/TheColaBro/vocabfloat/releases/download/v#{version}/VocabFloat.zip"
+  url "https://github.com/TheColaBro/Vocabfloat/releases/download/v#{version}/VocabFloat.zip"
   name "VocabFloat"
   desc "Minimalist macOS companion for global vocabulary lookup and PDF highlight extraction"
-  homepage "https://github.com/TheColaBro/vocabfloat"
+  homepage "https://github.com/TheColaBro/Vocabfloat"
 
   depends_on macos: :sonoma
 
